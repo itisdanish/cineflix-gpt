7a7715cbe87f9eba7894760cdb16477e24375b5d
itisdanish <ardentdew@gmail.com>
2025-09-06 09:36:11 +0530

    Logo Size fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
   return (
     <div className='absolute top-0 left-0 w-full bg-gradient-to-b from-black/80 to-transparent p-4 z-10'>
       <img
-        className='w-32 md:w-40 lg:w-48 object-contain'
+        className='w-32 md:w-32 lg:w-40 object-contain'
         src='https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png'
         alt='logo'
       />
